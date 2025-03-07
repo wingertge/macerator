631511393ca9d368b066ac4e73b4158fd6a0219d
Genna Wingert <1058083+wingertge@users.noreply.github.com>
2025-03-07 20:05:39 +0100

    Add bitwise binary ops

--- src/tests/arithmetic.rs
+++ src/tests/arithmetic.rs
@@ -1,53 +1,16 @@
 use crate::scalar::Fallback;
 
-use core::{fmt::Debug, marker::PhantomData};
+use core::fmt::Debug;
 
 use approx::{assert_relative_eq, RelativeEq};
 use bytemuck::Zeroable;
 use half::f16;
 use num_traits::NumCast;
 use paste::paste;
-use rand::{
-    distributions::{uniform::SampleUniform, Uniform},
-    Rng,
-};
 
-use crate::{
-    vload_unaligned, vstore_unaligned, Scalar, Simd, VAdd, VDiv, VMul, VMulAdd, VSub, Vector,
-};
+use crate::{vload_unaligned, vstore_unaligned, Simd, VAdd, VDiv, VMul, VMulAdd, VSub, Vector};
 
-trait Binop<T: Scalar> {
-    fn call<S: Simd>(lhs: Vector<S, T>, rhs: Vector<S, T>) -> Vector<S, T>;
-}
-
-#[inline(always)]
-fn test_binop<S: Simd, T: Scalar, Op: Binop<T>>(lhs: &[T], rhs: &[T]) -> Vec<T> {
-    let lanes = T::lanes::<S>();
-    let mut output = vec![Zeroable::zeroed(); lhs.len()];
-    let lhs = lhs.chunks_exact(lanes);
-    let rhs = rhs.chunks_exact(lanes);
-    let out = output.chunks_exact_mut(lanes);
-    for ((lhs, rhs), out) in lhs.zip(rhs).zip(out) {
-        let lhs = unsafe { vload_unaligned(lhs.as_ptr()) };
-        let rhs = unsafe { vload_unaligned(rhs.as_ptr()) };
-        unsafe { vstore_unaligned(out.as_mut_ptr(), Op::call::<S>(lhs, rhs)) };
-    }
-    output
-}
-
-macro_rules! binop {
-    ($trait: ident, $impl: expr) => {
-        paste! {
-            struct [<$trait Op>]<T>(PhantomData<T>);
-            impl<T: $trait> Binop<T> for [<$trait Op>]<T> {
-                #[inline(always)]
-                fn call<S: Simd>(lhs: Vector<S, T>, rhs: Vector<S, T>) -> Vector<S, T> {
-                    $impl(lhs, rhs)
-                }
-            }
-        }
-    };
-}
+use super::{binop, test_binop, testgen_binop, Binop};
 
 #[inline(always)]
 fn test_add_impl<S: Simd, T: VAdd>(lhs: &[T], rhs: &[T]) -> Vec<T> {
@@ -84,54 +47,6 @@ fn test_fma_impl<S: Simd, T: VMulAdd>(a: &[T], b: &[T], c: &[T]) -> Vec<T> {
         unsafe { vstore_unaligned(out.as_mut_ptr(), a.mul_add(b, c)) };
     }
     output
-}
-
-const SIZE: usize = 1024;
-fn random<T: SampleUniform>(lo: T, hi: T) -> Vec<T> {
-    let distribution = Uniform::new(lo, hi);
-    rand::thread_rng()
-        .sample_iter(&distribution)
-        .take(SIZE)
-        .collect()
-}
-
-macro_rules! testgen_binop {
-    ($test_fn: ident, $reference: expr, $($ty: ty),*) => {
-        $(paste! {
-            #[test]
-            fn [<$test_fn _ $ty>]() {
-                let lhs = random(NumCast::from(8).unwrap(), NumCast::from(16).unwrap());
-                let rhs = random(NumCast::from(0).unwrap(), NumCast::from(8).unwrap());
-                let out_ref = lhs
-                    .iter()
-                    .zip(rhs.iter())
-                    .map(|(a, b)| $reference(a, b))
-                    .collect::<Vec<_>>();
-                #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
-                {
-                    use crate::backend::x86::{v2::V2, v3::V3};
-                    if V3::is_available() {
-                        let out = V3::run_vectorized(|| [<$test_fn _impl>]::<V3, $ty>(&lhs, &rhs));
-                        assert_eq!(out_ref, out);
-                    }
-                    if V2::is_available() {
-                        let out = V2::run_vectorized(|| [<$test_fn _impl>]::<V2, $ty>(&lhs, &rhs));
-                        assert_eq!(out_ref, out);
-                    }
-                }
-                #[cfg(target_arch = "aarch64")]
-                {
-                    use crate::backend::aarch64::NeonFma;
-                    if NeonFma::is_available() {
-                        let out = NeonFma::run_vectorized(|| [<$test_fn _impl>]::<NeonFma, $ty>(&lhs, &rhs));
-                        assert_eq!(out_ref, out);
-                    }
-                }
-                let out = [<$test_fn _impl>]::<Fallback, $ty>(&lhs, &rhs);
-                assert_eq!(out_ref, out);
-            }
-        })*
-    };
 }
 
 testgen_binop!(
@@ -191,9 +106,9 @@ macro_rules! testgen_fma {
         $(paste! {
             #[test]
             fn [<$test_fn _ $ty>]() {
-                let a = random(NumCast::from(0).unwrap(), NumCast::from(8).unwrap());
-                let b = random(NumCast::from(0).unwrap(), NumCast::from(8).unwrap());
-                let c = random(NumCast::from(0).unwrap(), NumCast::from(64).unwrap());
+                let a = super::random(NumCast::from(0).unwrap(), NumCast::from(8).unwrap());
+                let b = super::random(NumCast::from(0).unwrap(), NumCast::from(8).unwrap());
+                let c = super::random(NumCast::from(0).unwrap(), NumCast::from(64).unwrap());
                 let out_ref = a
                     .iter()
                     .zip(b.iter()).zip(c.iter())
--- src/tests/mod.rs
+++ src/tests/mod.rs
@@ -1,1 +1,95 @@
+use bytemuck::Zeroable;
+use rand::{
+    distributions::{uniform::SampleUniform, Uniform},
+    Rng,
+};
+
+use crate::{vload_unaligned, vstore_unaligned, Scalar, Simd, Vector};
+
 mod arithmetic;
+mod bitwise;
+
+const SIZE: usize = 1024;
+fn random<T: SampleUniform>(lo: T, hi: T) -> Vec<T> {
+    let distribution = Uniform::new(lo, hi);
+    rand::thread_rng()
+        .sample_iter(&distribution)
+        .take(SIZE)
+        .collect()
+}
+
+macro_rules! testgen_binop {
+    ($test_fn: ident, $reference: expr, $($ty: ty),*) => {
+        $(::paste::paste! {
+            #[test]
+            fn [<$test_fn _ $ty>]() {
+                use num_traits::NumCast;
+
+                let lhs = $crate::tests::random(NumCast::from(8).unwrap(), NumCast::from(16).unwrap());
+                let rhs = $crate::tests::random(NumCast::from(0).unwrap(), NumCast::from(8).unwrap());
+                let out_ref = lhs
+                    .iter()
+                    .zip(rhs.iter())
+                    .map(|(a, b)| $reference(a, b))
+                    .collect::<Vec<_>>();
+                #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
+                {
+                    use $crate::backend::x86::{v2::V2, v3::V3};
+                    if V3::is_available() {
+                        let out = V3::run_vectorized(|| [<$test_fn _impl>]::<V3, $ty>(&lhs, &rhs));
+                        assert_eq!(out_ref, out);
+                    }
+                    if V2::is_available() {
+                        let out = V2::run_vectorized(|| [<$test_fn _impl>]::<V2, $ty>(&lhs, &rhs));
+                        assert_eq!(out_ref, out);
+                    }
+                }
+                #[cfg(target_arch = "aarch64")]
+                {
+                    use $crate::backend::aarch64::NeonFma;
+                    if NeonFma::is_available() {
+                        let out = NeonFma::run_vectorized(|| [<$test_fn _impl>]::<NeonFma, $ty>(&lhs, &rhs));
+                        assert_eq!(out_ref, out);
+                    }
+                }
+                let out = [<$test_fn _impl>]::<$crate::backend::scalar::Fallback, $ty>(&lhs, &rhs);
+                assert_eq!(out_ref, out);
+            }
+        })*
+    };
+}
+pub(crate) use testgen_binop;
+
+pub(crate) trait Binop<T: Scalar> {
+    fn call<S: Simd>(lhs: Vector<S, T>, rhs: Vector<S, T>) -> Vector<S, T>;
+}
+
+#[inline(always)]
+fn test_binop<S: Simd, T: Scalar, Op: Binop<T>>(lhs: &[T], rhs: &[T]) -> Vec<T> {
+    let lanes = T::lanes::<S>();
+    let mut output = vec![Zeroable::zeroed(); lhs.len()];
+    let lhs = lhs.chunks_exact(lanes);
+    let rhs = rhs.chunks_exact(lanes);
+    let out = output.chunks_exact_mut(lanes);
+    for ((lhs, rhs), out) in lhs.zip(rhs).zip(out) {
+        let lhs = unsafe { vload_unaligned(lhs.as_ptr()) };
+        let rhs = unsafe { vload_unaligned(rhs.as_ptr()) };
+        unsafe { vstore_unaligned(out.as_mut_ptr(), Op::call::<S>(lhs, rhs)) };
+    }
+    output
+}
+
+macro_rules! binop {
+    ($trait: ident, $impl: expr) => {
+        ::paste::paste! {
+            struct [<$trait Op>]<T>(::core::marker::PhantomData<T>);
+            impl<T: $trait> Binop<T> for [<$trait Op>]<T> {
+                #[inline(always)]
+                fn call<S: Simd>(lhs: Vector<S, T>, rhs: Vector<S, T>) -> Vector<S, T> {
+                    $impl(lhs, rhs)
+                }
+            }
+        }
+    };
+}
+pub(crate) use binop;

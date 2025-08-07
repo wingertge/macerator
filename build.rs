0383bd4ca3345f3006c66a07e53214bb659299c5
Genna Wingert <1058083+wingertge@users.noreply.github.com>
2025-08-07 18:05:37 +0200

    fix: Remove avx512 from aliases

--- build.rs
+++ build.rs
@@ -24,7 +24,6 @@ fn main() {
 
     cfg_aliases! {
         x86: { any(target_arch = "x86", target_arch = "x86_64") },
-        avx512: { all(target_arch = "x86_64", feature = "avx512") },
         fp16: { all(target_arch = "x86_64", feature = "fp16", feature = "nightly") },
         aarch64: { target_arch = "aarch64" },
         wasm32: { target_arch = "wasm32" },
